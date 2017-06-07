835db691e5b4e6fb89e4f8b2cfbf462bbf30b364
kmlee <km.lee@outlook.kr>
2017-06-07 11:16:13 +0900

    lr policy

--- examples/relu_ssd/relu_ssd_300_pascal.py
+++ examples/relu_ssd/relu_ssd_300_pascal.py
@@ -5,7 +5,7 @@ import relu_ssd_module as module
 if __name__ == "__main__":
     
     param = {
-        'net_name': "RELU_SSD_DECONV",
+        'net_name': "RUN_3WAY",
         'input_dim': 300,
         'pretrain_model': "models/VGGNet/VGG_ILSVRC_16_layers_fc_reduced.caffemodel",
         'data_set': "VOC0712",
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         'num_test_image': 4952,
         'test_batch_size': 8,
         'residual_feature_depth': 256,
-        'use_res_branch2' : False,
+        'use_res_branch2' : True,
         'use_res_deconv' : True,
 
 
@@ -26,10 +26,10 @@ if __name__ == "__main__":
             # Train parameters            
             'weight_decay': 0.0005,
             'lr_policy': "multistep",
-            'stepvalue': [80000, 120000, 140000],
+            'stepvalue': [80000, 110000, 140000],
             'gamma': 0.1,
             'momentum': 0.9,            
-            'max_iter': 140000,
+            'max_iter': 120000,
             'snapshot': 10000,
             'display': 10,
             'average_loss': 10,
